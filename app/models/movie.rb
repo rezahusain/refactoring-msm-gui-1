90677faef46222cb5fc6bfdaa29db277b12746e3
rezahusain <rezahusain99@gmail.com>
2024-06-13 11:35:30 -0500

    Refactored movie director

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,4 +13,13 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+  def director
+    the_id = self.director_id
+
+    matching_directors = Director.where({ :id => the_id })
+        
+    the_director = matching_directors.at(0)
+
+    return the_director.name
+  end
 end
